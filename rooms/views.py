104f4303081c3572dc260832d85eae694e48e40f
SeolHuiGwan9478 <shg5977@naver.com>
2022-06-03 23:17:04 +0900

    [feat] : Implementation room.views by using APIView

--- rooms/views.py
+++ rooms/views.py
@@ -1,10 +1,10 @@
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Room
 from .serializers import RoomSerializer
 # Create your views here
-@api_view(["GET"])
-def list_rooms(request):
-    rooms = Room.objects.all()
-    serialized_rooms = RoomSerializer(rooms, many=True)
-    return Response(data=serialized_rooms.data)
+class ListRoomView(APIView):
+    def get(self, request):
+        rooms = Room.objects.all()
+        serializer = RoomSerializer(rooms, many=True)
+        return Response(data=rooms)
