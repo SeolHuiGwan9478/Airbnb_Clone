edffa76edf4c44ccf5ae830ce630f372550d79ff
SeolHuiGwan9478 <shg5977@naver.com>
2022-06-29 01:09:08 +0900

    [feat]: add permission_classes, write WriteUserSerializer function

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -12,42 +12,13 @@ class ReadRoomSerializer(serializers.ModelSerializer):
         )
 
 class WriteRoomSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=140)
-    address = serializers.CharField(max_length=140)
-    price = serializers.IntegerField()
-    beds = serializers.IntegerField(default=1)
-    lat = serializers.DecimalField(max_digits=10, decimal_places=6)
-    lng = serializers.DecimalField(max_digits=10, decimal_places=6)
-    bedrooms = serializers.IntegerField(default=1)
-    bathrooms = serializers.IntegerField(default=1)
-    check_in = serializers.TimeField(default="00:00:00")
-    check_out = serializers.TimeField(default="00:00:00")
-    instant_book = serializers.BooleanField(default=False)
-
-    def create(self, validated_data):
-        return Room.objects.create(**validated_data)
-    
-    # def validate_beds(self, beds):
-    #     if beds < 5:
-    #         raise serializers.ValidationError("Your house is too small")
-    #     else:
-    #         return beds
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", default = instance.name)
-        instance.address = validated_data.get("address", default = instance.address)
-        instance.price = validated_data.get("price", default = instance.price)
-        instance.beds = validated_data.get("beds", default = instance.beds)
-        instance.lat = validated_data.get("lat", default = instance.lat)
-        instance.lng = validated_data.get("lng", default = instance.lng)
-        instance.bedrooms = validated_data.get("bedrooms", default = instance.bedrooms)
-        instance.bathrooms = validated_data.get("bathrooms", default = instance.bathrooms)
-        instance.check_in = validated_data.get("check_in", default = instance.check_in)
-        instance.check_out = validated_data.get("check_out", default = instance.check_out)
-        instance.instant_book = validated_data.get("instant_book", default = instance.instant_book)
-
-        instance.save()
-        return instance
+    class Meta:
+        model = Room
+        exclude = (
+            "user",
+            "modified",
+            "created",
+        )
 
     def validate(self, data):
         if self.instance:
--- users/views.py
+++ users/views.py
@@ -2,17 +2,23 @@ from rest_framework import status
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view
-from .serializers import ReadUserSerializer, RelatedUserSerializer
+from .serializers import ReadUserSerializer, RelatedUserSerializer, WriteUserSerializer
 from .models import User
 # Create your views here.
 class MeView(APIView):
+    permission_classes = [IsAuthenticated]
     def get(self, request):
-        if request.user.is_authenticated:
-            return Response(RelatedUserSerializer(request.user).data)
+        return Response(ReadUserSerializer(request.user).data)
     
     def put(self, request):
-        pass
+        serializer = WriteUserSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response()
+        else:
+            return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
 
 @api_view(["GET"])
 def user_detail(request, pk):
