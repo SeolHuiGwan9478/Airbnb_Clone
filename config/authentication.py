8d82c8a6121816e30043cc8c14d617b374fa03ee
SeolHuiGwan9478 <shg5977@naver.com>
2022-06-29 22:12:51 +0900

    [feat] : add JWT func

---
+++ config/authentication.py
@@ -0,0 +1,21 @@
+from base64 import decode
+from django.contrib.auth.models import User
+from rest_framework import authentication
+from rest_framework import exceptions
+from django.conf import settings
+import jwt
+from users.models import User
+
+class JWTAuthentication(authentication.BaseAuthentication):
+    def authenticate(self, request):
+        try:
+            token = request.META.get('HTTP_AUTHORIAZATION')
+            if token is None:
+                return None
+            xjwt, jwt_token = token.split(" ")
+            decoded = jwt.decode(jwt_token, settings.SECRET_KEY, algorithms=["HS256"])
+            pk = decoded.get("pk")
+            user = User.objects.get(pk=pk)
+            return (user, None)
+        except (ValueError, jwt.exceptions.DecodeError):
+            return None
