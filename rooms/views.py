1b006aa35aba5d548df9ae80e2ff493c247d88a8
SeolHuiGwan9478 <shg5977@naver.com>
2022-06-29 15:03:38 +0900

    [feat] : add favs function

--- rooms/views.py
+++ rooms/views.py
@@ -3,23 +3,23 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from .models import Room
-from .serializers import ReadRoomSerializer, WriteRoomSerializer
+from .serializers import RoomSerializer, RoomSerializer
 # Create your views here
 
 class RoomsView(APIView):
     # GET
-    def get(request):
+    def get(self,request):
         rooms = Room.objects.all()[:5]
-        serializer = ReadRoomSerializer(rooms, many=True).data
+        serializer = RoomSerializer(rooms, many=True).data
         return Response(data=serializer)
     #POST
-    def post(request):
+    def post(self,request):
         if not request.user.is_authenticated:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
-        serializer = WriteRoomSerializer(data=request.data)
+        serializer = RoomSerializer(data=request.data)
         if serializer.is_valid():
             room = serializer.save(user=request.user)
-            room_serializer = ReadRoomSerializer(room)
+            room_serializer = RoomSerializer(room)
             return Response(data=room_serializer, status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
@@ -35,7 +35,7 @@ class RoomView(APIView):
     def get(self, request, pk):
         room = self.get_room(pk)
         if room is not None:
-            serializer = ReadRoomSerializer(room).data
+            serializer = RoomSerializer(room).data
             return Response(serializer)
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
@@ -45,7 +45,7 @@ class RoomView(APIView):
         if room is not None:
             if room.user != request.user:
                 return Response(status=status.HTTP_401_UNAUTHORIZED)
-            serializer = WriteRoomSerializer(instance=room, data=request.data, partial=True)
+            serializer = RoomSerializer(instance=room, data=request.data, partial=True)
             if serializer.is_valid():
                 room = serializer.save()
                 return Response(room, status=status.HTTP_200_OK)
