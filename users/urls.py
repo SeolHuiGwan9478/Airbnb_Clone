f8ec5da6e3efc7f5bd76f8ed73515e0752a44dac
SeolHuiGwan9478 <shg5977@naver.com>
2022-07-07 19:30:34 +0900

    [feat] : complete login or user func by using Viewset

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+
 app_name = "users"
 
-urlpatterns = [
-    path("me/", views.MeView.as_view()),
-    path("me/favs/", views.FavsView.as_view()),
-    path("<int:pk>", views.user_detail),
-]
+router = DefaultRouter()
+router.register("", views.UserViewset)
+urlpatterns = router.urls
--- users/views.py
+++ users/views.py
@@ -6,34 +6,45 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import api_view
+from rest_framework.decorators import action
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser, AllowAny
 from .serializers import UserSerializer
 from rooms.serializers import RoomSerializer
 from rooms.models import Room
 from .models import User
+from .permissions import IsSelf
 # Create your views here.
+class UserViewset(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-class UserView(APIView):
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            new_user = serializer.save() #시리얼라이저의 create 호출
-            return Response(UserSerializer(new_user).data)
+    def get_permissions(self):
+        permission_classes = [] # 임시 permssion 클래스
+        if self.action == "list":
+            permission_classes = [IsAdminUser]
+        elif self.action == "create" or self.action == "retreive":
+            permission_classes = [AllowAny]
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class MeView(APIView):
-    permission_classes = [IsAuthenticated]
-    def get(self, request):
-        return Response(UserSerializer(request.user).data)
+            permission_classes = [IsSelf]
+        return [permission() for permission in permission_classes]
     
-    def put(self, request):
-        serializer = UserSerializer(request.user, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response()
+    @action(detail=False, methods=["post"])
+    def login(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            encoded_jwt = jwt.encode(
+                {"id": user.pk},
+                settings.SECRET_KEY,
+                algorithm="HS256"
+            )
+            return Response(data={"token" : encoded_jwt, "id":user.pk})
         else:
-            return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 class FavsView(APIView):
 
@@ -60,27 +71,4 @@ class FavsView(APIView):
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(["GET"])
-def user_detail(request, pk):
-    try:
-        user = User.objects.get(pk=pk)
-        return Response(UserSerializer(user).data)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
     
-@api_view(["POST"])
-def login(request):
-    username = request.data.get("username")
-    password = request.data.get("password")
-    if not username or not password:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        encoded_jwt = jwt.encode(
-            {"id": user.pk},
-            settings.SECRET_KEY,
-            algorithm="HS256"
-        )
-        return Response(data={"token" : encoded_jwt})
-    else:
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
